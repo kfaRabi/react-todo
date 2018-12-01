bafe6f627969f3f316a5652d199b51da6673514b
subratopal79@gmail.com <mridul.link@gmail.com>
2018-12-01 17:12:01 +0600

    Implement counter app

--- public/script/app.js
+++ public/script/app.js
@@ -1,53 +1,58 @@
 "use strict";
 
-var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        { id: "par" },
-        "Hello World"
-    ),
-    React.createElement(
-        "p",
-        null,
-        "Para"
-    )
-);
-
-var addOne = function addOne() {
-    console.log("Added One");
-};
-
-// Add function to handle -1 button event
+// const template = (
+//     <div>
+//         <h1 id="par">Hello World</h1>
+//         <p>Para</p>
+//     </div>
+// )
 
 var count = 0;
 
-var counterTemplate = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        { id: "counter", className: "counter" },
-        count
-    ),
-    React.createElement(
-        "button",
-        { onClick: addOne },
-        "+1"
-    ),
-    React.createElement(
-        "button",
-        null,
-        "-1"
-    ),
-    React.createElement(
-        "button",
-        null,
-        "Reset"
-    )
-);
+var addOne = function addOne() {
+    count++;
+    render();
+};
 
-var myApp = document.querySelector("#my-app");
+// Add function to handle -1 button event
+var deductOne = function deductOne() {
+    count--;
+    render();
+};
 
-ReactDOM.render(counterTemplate, myApp);
+var resetCounter = function resetCounter() {
+    count = 0;
+    render();
+};
+
+var render = function render() {
+    var counterTemplate = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            { id: "counter", className: "counter" },
+            count
+        ),
+        React.createElement(
+            "button",
+            { onClick: addOne },
+            "+1"
+        ),
+        React.createElement(
+            "button",
+            { onClick: deductOne },
+            "-1"
+        ),
+        React.createElement(
+            "button",
+            { onClick: resetCounter },
+            "Reset"
+        )
+    );
+
+    var myApp = document.querySelector("#my-app");
+
+    ReactDOM.render(counterTemplate, myApp);
+};
+render();
--- src/srcApp.js
+++ src/srcApp.js
@@ -1,27 +1,40 @@
-const template = (
-    <div>
-        <h1 id="par">Hello World</h1>
-        <p>Para</p>
-    </div>
-)
-
-const addOne = () => {
-    console.log("Added One");
-}
-
-// Add function to handle -1 button event
+// const template = (
+//     <div>
+//         <h1 id="par">Hello World</h1>
+//         <p>Para</p>
+//     </div>
+// )
 
 let count = 0;
 
-const counterTemplate = (
-    <div>
-        <h1 id="counter" className="counter">{count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button>-1</button>
-        <button>Reset</button>
-    </div>
-);
+const addOne = () => {
+    count++;
+    render();
+}
 
-const myApp = document.querySelector("#my-app");
+// Add function to handle -1 button event
+const deductOne = ()=>{
+    count--;
+    render();
+}
 
-ReactDOM.render(counterTemplate, myApp);
+const resetCounter = ()=>{
+    count = 0;
+    render();
+}
+
+const render = () => {
+    const counterTemplate = (
+        <div>
+            <h1 id="counter" className="counter">{count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={deductOne}>-1</button>
+            <button onClick={resetCounter}>Reset</button>
+        </div>
+    );
+    
+    const myApp = document.querySelector("#my-app");
+    
+    ReactDOM.render(counterTemplate, myApp);
+}
+render();
