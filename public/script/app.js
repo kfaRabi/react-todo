cbfb901c343a4dcef904154af3e573b74c50de81
subratopal79@gmail.com <mridul.link@gmail.com>
2018-12-01 19:17:29 +0600

    Components

--- public/script/app.js
+++ public/script/app.js
@@ -2,120 +2,97 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var Header = function (_React$Component) {
+    _inherits(Header, _React$Component);
 
-var Person = function () {
-    function Person(name, age) {
-        _classCallCheck(this, Person);
+    function Header() {
+        _classCallCheck(this, Header);
 
-        this.name = name;
-        this.age = age;
+        return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
     }
 
-    _createClass(Person, [{
-        key: "getDescription",
-        value: function getDescription() {
-            return this.name + " is " + this.age + " years old";
-        }
-    }]);
-
-    return Person;
-}();
-
-var Student = function (_Person) {
-    _inherits(Student, _Person);
-
-    function Student(name, age, ID) {
-        _classCallCheck(this, Student);
-
-        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
-
-        _this.ID = ID;
-        return _this;
-    }
-
-    _createClass(Student, [{
-        key: "getDescription",
-        value: function getDescription() {
-            return this.name + " is " + this.age + " years old and his id is " + this.ID;
-        }
-    }]);
-
-    return Student;
-}(Person);
-
-var p = new Person("John", 30);
-console.log(p.getDescription());
-
-var p2 = new Person("Jane", 25);
-console.log(p2.getDescription());
-
-var stud = new Student("Dave", 19, "2013222");
-console.log(stud.getDescription());
-
-var items = ["Go to the market", "Clean room"];
-
-var handleFormSubmit = function handleFormSubmit(e) {
-    e.preventDefault();
-    var val = e.target.elements.task.value;
-
-    if (val) {
-        items.push(val);
-        e.target.elements.task.value = "";
-        render();
-    }
-};
-
-var handleClear = function handleClear() {
-    items = [];
-    render();
-};
-
-var render = function render() {
-
-    var template = React.createElement(
-        "div",
-        null,
-        React.createElement(
-            "h1",
-            null,
-            "App Title"
-        ),
-        React.createElement(
-            "ol",
-            null,
-            items.map(function (item) {
-                return React.createElement(
-                    "li",
-                    { key: item },
-                    item
-                );
-            })
-        ),
-        React.createElement(
-            "button",
-            { onClick: handleClear, disabled: items.length === 0 ? true : false },
-            "Clear"
-        ),
-        React.createElement(
-            "form",
-            { onSubmit: handleFormSubmit },
-            React.createElement("input", { type: "text", name: "task" }),
-            React.createElement(
-                "button",
+    _createClass(Header, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
                 null,
-                "Add Item"
-            )
-        )
-    );
+                React.createElement(
+                    "h1",
+                    null,
+                    this.props.title
+                )
+            );
+        }
+    }]);
 
-    var myApp = document.querySelector("#my-app");
+    return Header;
+}(React.Component);
 
-    ReactDOM.render(template, myApp);
-};
+var Item = function (_React$Component2) {
+    _inherits(Item, _React$Component2);
 
-render();
+    function Item() {
+        _classCallCheck(this, Item);
+
+        return _possibleConstructorReturn(this, (Item.__proto__ || Object.getPrototypeOf(Item)).apply(this, arguments));
+    }
+
+    _createClass(Item, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "li",
+                null,
+                "Item 1"
+            );
+        }
+    }]);
+
+    return Item;
+}(React.Component);
+
+var Items = function (_React$Component3) {
+    _inherits(Items, _React$Component3);
+
+    function Items() {
+        _classCallCheck(this, Items);
+
+        return _possibleConstructorReturn(this, (Items.__proto__ || Object.getPrototypeOf(Items)).apply(this, arguments));
+    }
+
+    _createClass(Items, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "ol",
+                    null,
+                    React.createElement(Item, null),
+                    React.createElement(Item, null),
+                    React.createElement(Item, null),
+                    React.createElement(Item, null)
+                )
+            );
+        }
+    }]);
+
+    return Items;
+}(React.Component);
+
+var template = React.createElement(
+    "div",
+    null,
+    React.createElement(Header, { title: "Best App" }),
+    React.createElement(Items, null)
+);
+
+ReactDOM.render(template, document.getElementById("app"));
--- src/srcApp.js
+++ src/srcApp.js
@@ -1,79 +1,42 @@
-class Person{
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    getDescription() {
-        return this.name + " is " + this.age + " years old";
-    }
-
-}
-
-class Student extends Person {
-    constructor(name, age, ID) {
-        super(name, age);
-        this.ID = ID;
-    }
-
-    getDescription() {
-        return this.name + " is " + this.age + " years old and his id is " + this.ID;        
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+            </div>
+        )
     }
 }
 
-const p = new Person("John", 30);
-console.log(p.getDescription());
 
-const p2 = new Person("Jane", 25);
-console.log(p2.getDescription());
-
-const stud = new Student("Dave", 19, "2013222");
-console.log(stud.getDescription());
-
-
-let items = ["Go to the market", "Clean room"];
-
-const handleFormSubmit = (e) => {
-    e.preventDefault();
-    const val = e.target.elements.task.value;
-
-    if(val) {
-        items.push(val);
-        e.target.elements.task.value = "";
-        render();
+class Item extends React.Component {
+    render() {
+        return (
+            <li>Item 1</li>
+        )
+    }
+}
+class Items extends React.Component {
+    render() {
+        return (
+            <div>
+                <ol>
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                </ol>
+            </div>
+        )
     }
 }
 
-const handleClear = () => {
-    items = [];
-    render();
-}
 
+const template = (
+    <div>
+        <Header title="Best App" />
+        <Items />
+    </div>
+);
 
-
-
-const render = () => {
- 
-    const template = (
-        <div>
-            <h1>App Title</h1>
-
-            <ol>
-                {items.map(item => <li key={item}>{item}</li>)}
-            </ol>
-
-            <button onClick={handleClear} disabled={items.length === 0 ? true : false}>Clear</button>
-
-            <form onSubmit={handleFormSubmit}>
-                <input type="text"  name="task"/>
-                <button>Add Item</button>
-            </form>
-        </div>
-    )
-
-    const myApp = document.querySelector("#my-app");
-    
-    ReactDOM.render(template, myApp);
-}
-
-render();
+ReactDOM.render(template, document.getElementById("app"));
